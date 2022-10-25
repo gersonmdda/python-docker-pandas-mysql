9ffa0a87c0da8e02d6d791333cb0148227152931
gersonmdda <gersonmdda@gmail.com>
2022-10-25 13:40:23 -0300

    mysql

--- app.py
+++ app.py
@@ -3,10 +3,28 @@ import pandas as pd
 import redis
 from flask import Flask
 import logging
+import mysql.connectors
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 logging.basicConfig(level=logging.DEBUG)
+mydb = mysql.connector.connect(
+  host="0.0.0.0:3388",
+  user="usuario",
+  password="123456",
+  database="game"
+)
+
+#ENV MYSQL_HOST=0.0.0.0:3388
+#ENV MYSQL_USER=usuario
+#ENV MYSQL_PASSWORD=123456
+#ENV MYSQL_DATABADE=game
+
+def connectMysql(row):
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO games (jogo, console, status) VALUES (%s, %s, %s, %s)"
+    val = (row.Jogo, row.Consoles, row.Status)
+    mycursor.execute(sql, val)
 
 
 def readCsv():
@@ -14,7 +32,7 @@ def readCsv():
     #for x in df:
         #   app.logger.info(df.iloc[2,2])
     for index, row in df.iterrows():
-            #app.logger.info(row.Jogo)
+        connectMysql(row)
     return df.to_string()
 
 @app.route('/')
---
+++ table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE games (
+	id int auto_increment primary key,
+    jogo varchar(255),
+    consoles varchar(255),
+    status varchar(255)
+);
